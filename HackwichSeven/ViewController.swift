da91dc46f2343f42ac7305c96e38f149637355e0
CM Student <cmstudent@uhwo-acm-c-14.local>
2020-03-05 10:27:33 -1000

    Hackwich 7 (Part 1-8)

--- HackwichSeven/ViewController.swift
+++ HackwichSeven/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var displaylabel: UITextField!
+    
+    @IBOutlet weak var Label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        self.Label.text=""
+    
+        self.view.backgroundColor = UIColor.red
+    
     }
 
-
+    @IBAction func ButtonPressd(_ sender: Any) {
+    
+        let userInputText = displaylabel.text
+        
+        //display user text in displaylabel
+        
+        self.Label.text = userInputText
+    }
+    
+    
 }
 
